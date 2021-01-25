41f16f972602aef40b29cf563ee1327d8e9f31d9
Will Lane <williamlane923@gmail.com>
2021-01-25 13:23:28 -0800

    added basic library layout and boilerplate to get you started

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+pub fn OwOify(input_text: String, level: OwoLevel) -> String {
+	match level {
+    OwoLevel::Low => {
+
+      "low return".parse().unwrap()
+    },
+    OwoLevel::Medium => {
+
+      "medium return".parse().unwrap()
+    },
+    OwoLevel::High => {
+
+      "high return".parse().unwrap()
+    }
+  }
+}
+
+pub enum OwoLevel {
+    Low,
+    Medium,
+    High
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn testowo() {
+	    assert_eq!(OwOify(String::from("hello"), OwoLevel::Low), String::from("low return"));
+      assert_eq!(OwOify(String::from("hello"), OwoLevel::Medium), String::from("medium return"));
+      assert_eq!(OwOify(String::from("hello"), OwoLevel::High), String::from("high return"));
+    }
+}
