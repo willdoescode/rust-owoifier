543530a835d523a111c2025e5b5693d8e131141e
Will Lane <williamlane923@gmail.com>
2021-01-25 13:30:39 -0800

    made everything double spaced instead of quadruple spaced

--- src/lib.rs
+++ src/lib.rs
@@ -16,20 +16,20 @@ pub fn OwOify(input_text: String, level: OwoLevel) -> String {
 }
 
 pub enum OwoLevel {
-    Low,
-    Medium,
-    High
+  Low,
+  Medium,
+  High
 }
 
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn testowo() {
-      assert_eq!(OwOify(String::from("hello"), OwoLevel::Low), String::from("low return"));
-      assert_eq!(OwOify(String::from("hello"), OwoLevel::Medium), String::from("medium return"));
-      assert_eq!(OwOify(String::from("hello"), OwoLevel::High), String::from("high return"));
-    }
+  #[test]
+  fn testowo() {
+    assert_eq!(OwOify(String::from("hello"), OwoLevel::Low), String::from("low return"));
+    assert_eq!(OwOify(String::from("hello"), OwoLevel::Medium), String::from("medium return"));
+    assert_eq!(OwOify(String::from("hello"), OwoLevel::High), String::from("high return"));
+  }
 }
